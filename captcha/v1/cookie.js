dd190b5c140d9cfbda136cd0ce954a127238c077
Dev.Huang <18778418865@163.com>
2024-07-04 15:26:09 +0800

    Create cookie.js

---
+++ captcha/v1/cookie.js
@@ -0,0 +1,15 @@
+function clearCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+}
+
+// clear cookie
+window.onload = function() {
+    clearCookies();
+}
