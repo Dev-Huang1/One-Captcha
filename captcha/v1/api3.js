91c0d5f86ff626149cd3b4bec213da28c7cb8920
Dev.Huang <18778418865@163.com>
2024-07-05 15:18:12 +0800

    Update api3.js

--- captcha/v1/api3.js
+++ captcha/v1/api3.js
@@ -12,7 +12,6 @@
             border-radius: 8px;
             display: flex;
             align-items: center;
-            justify-content: space-between;
             font-family: Arial, sans-serif;
             background-color: #ffffff;
             color: #000000;
