eff7be159d8d85b4fab99f502d79f7c603be751b
Dev.Huang <devhuang000@outlook.com>
2024-07-12 18:25:10 +0800

    Update api.js

--- captcha/v3-Developer-Beta/api.js
+++ captcha/v3-Developer-Beta/api.js
@@ -197,6 +197,36 @@ const cssStyles = `
     }
 `;
 
+const Air-captcha = `
+    <div id="captcha-container">
+        <div id="verify-section">
+            <input type="checkbox" id="verify-checkbox">
+            <img id="check-mark" src="assets/check-mark.svg" alt="Check Mark" style="display: none;">
+            <label for="verify-checkbox" id="captcha-label">I'm not a robot.</label>
+            <span id="success-message" style="display: none; font-weight: bold;">Success</span>
+        </div>
+        <div id="brand">
+            <a href="https://github.com/Dev-Huang1/Air-Captcha"><img src="https://captcha.xyehr.cn/assets/logo/logo.svg" alt="AIR Captcha Logo"></a>
+            Air Captcha
+            <div class="privacy-terms-links">
+                <a href="https://www.xyehr.cn/air-captcha-privacy-policy" id="privacy-link">Privacy</a><p>·</p><a href="https://help.xyehr.cn/jekyll/2024-07-05-air-captcha.html" id="docs-link">Docs</a>
+            </div>
+        </div>
+    </div>
+
+    <div id="slider-captcha">
+        <div id="puzzle-container">
+            <img id="puzzle-image" src="" alt="img">
+            <div id="puzzle-piece"></div>
+        </div>
+        <div id="slider">
+            <div id="slider-track"></div>
+            <div id="slider-handle">→</div>
+        </div>
+        <button id="retry-button">Retry</button>
+    </div>
+`
+
 // JavaScript代码
 const jsScript = `
     const verifyCheckbox = document.getElementById('verify-checkbox');
@@ -433,6 +463,10 @@ const jsScript = `
 const styleTag = document.createElement('style');
 styleTag.textContent = cssStyles;
 document.head.appendChild(styleTag);
+
+const divTag = document.createElement('div');
+divTag.textContent = cssStyles;
+document.head.appendChild(divTag);
 
 // 创建一个script标签并将JavaScript代码添加进去
 const scriptTag = document.createElement('script');
