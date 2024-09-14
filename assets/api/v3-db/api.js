679f005ad6a5aeca26b59a7e29a7c71707e128b1
Dev.Huang <devhuang000@outlook.com>
2024-09-14 22:57:54 +0800

    Update api.js

--- assets/api/v3-db/api.js
+++ assets/api/v3-db/api.js
@@ -252,30 +252,36 @@
     `;
 
     const captchaHTML = `
-        <div id="captcha-container">
-            <div id="verify-section">
-                <input type="checkbox" id="verify-checkbox">
-                <label for="verify-checkbox" class="custom-checkbox"></label>
-                <img id="check-mark" src="assets/check-mark.svg" alt="Check Mark" style="display: none;">
-                <label for="verify-checkbox" id="captcha-label">I'm not a robot.</label>
-                <span id="success-message" style="display: none;">Success</span>
-            </div>
-            <div id="brand">
-                <a href="https://github.com/Dev-Huang1/One-Captcha"><img src="https://captcha.xyehr.cn/assets/logo/logo.svg" alt="One Captcha Logo"></a>
-                One Captcha
+    <div id="captcha-container">
+        <div id="verify-section">
+            <input type="checkbox" id="verify-checkbox">
+            <label for="verify-checkbox" class="custom-checkbox"></label>
+            <div id="loading-spinner"></div>
+            <img id="check-mark" src="assets/check-mark.svg" alt="Check Mark" style="display: none;">
+            <label for="verify-checkbox" id="captcha-label">I'm not a robot.</label>
+            <span id="success-message" style="display: none;">Success</span>
+        </div>
+        <div id="brand">
+            <a href="https://github.com/Dev-Huang1/One-Captcha"><img src="https://captcha.xyehr.cn/assets/logo/logo.svg" alt="One Captcha Logo"></a>
+            One Captcha
+            <div class="privacy-terms-links">
+                <a href="https://www.xyehr.cn/one-captcha-privacy-policy" id="privacy-link">Privacy</a><p>·</p><a href="https://help.xyehr.cn/jekyll/2024-07-05-one-captcha.html" id="docs-link">Docs</a>
             </div>
         </div>
-        <div id="slider-captcha">
-            <div id="puzzle-container">
-                <img id="puzzle-image" src="" alt="img">
-                <div id="puzzle-piece"></div>
-            </div>
-            <div id="slider">
-                <div id="slider-track"></div>
-                <div id="slider-handle">→</div>
-            </div>
+    </div>
+
+    <div id="slider-captcha">
+        <div id="puzzle-container">
+            <img id="puzzle-image" src="" alt="img">
+            <div id="puzzle-piece"></div>
         </div>
-        <button id="submit-button" disabled>Submit</button>
+        <p id="error-message" style="color: red; display: none;">Verification failed. Please try again.</p>
+        <div id="slider">
+            <div id="slider-track"></div>
+            <div id="slider-handle">→</div>
+        </div>
+        <button id="retry-button">Retry</button>
+    </div>
     `;
 
     // Inject the template and styles into the page
