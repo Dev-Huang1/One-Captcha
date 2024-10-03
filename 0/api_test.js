c8c9511b93a10f4c781d6d5f1cec2f7cd8b6c621
Evan Huang <evan.huang000@proton.me>
2024-10-03 17:17:55 +0800

    Update api_test.js

--- 0/api_test.js
+++ 0/api_test.js
@@ -150,8 +150,7 @@ function OneCaptchaInit() {
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        width: 285px;
-        height: 350px;
+        width: 245px;
         background-color: #fff;
         border: 1px solid #ccc;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
